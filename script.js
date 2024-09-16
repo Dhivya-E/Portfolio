9484082a4686d5eacbcfb4d3df8bb8aa04612dc9
Dhivya-E <dhivyait25@gmail.com>
2024-09-16 16:39:26 +0530

    second commit

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 var cursor = {
     delay: 8,
     _x: 0,
@@ -9,35 +8,26 @@ var cursor = {
     cursorEnlarged: false,
     $dot: document.querySelector('.cursor-dot'),
     $outline: document.querySelector('.cursor-dot-outline'),
-    
+    isMobile: /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+
     init: function() {
+        if (this.isMobile) {
+            this.$dot.style.display = 'none';
+            this.$outline.style.display = 'none';
+            return; // Skip initialization on mobile devices
+        }
+
         // Set up element sizes
         this.dotSize = this.$dot.offsetWidth;
         this.outlineSize = this.$outline.offsetWidth;
-        
+
         this.setupEventListeners();
         this.animateDotOutline();
     },
-    
-//     updateCursor: function(e) {
-//         var self = this;
-        
-//         console.log(e)
-        
-//         // Show the cursor
-//         self.cursorVisible = true;
-//         self.toggleCursorVisibility();
 
-//         // Position the dot
-//         self.endX = e.pageX;
-//         self.endY = e.pageY;
-//         self.$dot.style.top = self.endY + 'px';
-//         self.$dot.style.left = self.endX + 'px';
-//     },
-    
     setupEventListeners: function() {
         var self = this;
-        
+
         // Anchor hovering
         document.querySelectorAll('a').forEach(function(el) {
             el.addEventListener('mouseover', function() {
@@ -49,7 +39,7 @@ var cursor = {
                 self.toggleCursorSize();
             });
         });
-        
+
         // Click events
         document.addEventListener('mousedown', function() {
             self.cursorEnlarged = true;
@@ -59,8 +49,7 @@ var cursor = {
             self.cursorEnlarged = false;
             self.toggleCursorSize();
         });
-  
-  
+
         document.addEventListener('mousemove', function(e) {
             // Show the cursor
             self.cursorVisible = true;
@@ -72,7 +61,7 @@ var cursor = {
             self.$dot.style.top = self.endY + 'px';
             self.$dot.style.left = self.endX + 'px';
         });
-        
+
         // Hide/show cursor
         document.addEventListener('mouseenter', function(e) {
             self.cursorVisible = true;
@@ -80,7 +69,7 @@ var cursor = {
             self.$dot.style.opacity = 1;
             self.$outline.style.opacity = 1;
         });
-        
+
         document.addEventListener('mouseleave', function(e) {
             self.cursorVisible = true;
             self.toggleCursorVisibility();
@@ -88,21 +77,21 @@ var cursor = {
             self.$outline.style.opacity = 0;
         });
     },
-    
+
     animateDotOutline: function() {
         var self = this;
-        
+
         self._x += (self.endX - self._x) / self.delay;
         self._y += (self.endY - self._y) / self.delay;
         self.$outline.style.top = self._y + 'px';
         self.$outline.style.left = self._x + 'px';
-        
+
         requestAnimationFrame(this.animateDotOutline.bind(self));
     },
-    
+
     toggleCursorSize: function() {
         var self = this;
-        
+
         if (self.cursorEnlarged) {
             self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)';
             self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)';
@@ -111,10 +100,10 @@ var cursor = {
             self.$outline.style.transform = 'translate(-50%, -50%) scale(1)';
         }
     },
-    
+
     toggleCursorVisibility: function() {
         var self = this;
-        
+
         if (self.cursorVisible) {
             self.$dot.style.opacity = 1;
             self.$outline.style.opacity = 1;
